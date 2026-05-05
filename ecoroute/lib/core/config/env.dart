68f432e3f40fdccb57ae0786f1edf031c07029c4
12khabdouabdou <zaaaabd7@gmail.com>
2026-05-05 22:30:51 +0000

    Create env.dart

---
+++ ecoroute/lib/core/config/env.dart
@@ -0,0 +1,4 @@
+class Env {
+  static const String supabaseUrl = 'https://jrfwxnwoeetqjbvicokb.supabase.co';
+  static const String supabaseAnonKey = 'sb_publishable_SL9JHkOxmz4qnY5VbWIY6A_bqtEZ0nx';
+}
